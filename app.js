80df42e25b410d1a2977d6fd53acc0009bbfcf65
Fernand DelaGuardia <f.delaguardia@yahoo.com>
2023-02-08 17:24:44 -0500

    lab

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var celebritiesRouter = require('./routes/celebrities.routes');
+var moviesRouter = require('./routes/movies.routes');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/celebrities', celebritiesRouter);
+app.use('/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,61 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movie = require('../models/Movie.model');
+const Celebrities = require('../models/Celebrity.model');
 
-// all your routes here
+router.get('/create', (req, res, next) => {
+    Celebrities.find()
+        .then((foundCel) => {
+            res.render('movies/new-movies.hbs', {foundCel})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+        
+})
+router.post('/create', (req, res, next) => {
+    Movie.create({
+        title: req.body.title,
+        genre: req.body.genre,
+        plot:   req.body.plot,
+        cast: req.body.cast,
+    })
+    .then((createMovies) => {
+        console.log(createMovies)
+        res.redirect('/movies')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+router.get('/', (req, res, next) => {
+   
+    Movie.find()
+    .then((foundMovies) => {
+        console.log(foundMovies)
+        res.render("movies/movies.hbs", {
+            foundMovies
+        })
+    })
+})
+
+router.get('/:id', (req, res, next) => {
+
+    const {id} = req.params
+    console.log(id)
+
+    Movie.findOne({id})
+        .populate("cast")
+        .then((foundMovie) => {
+            console.log(foundMovie)
+            res.render('movies/movie-details.hbs', {
+                foundMovie
+            })
+        })
+})
+
+
+
 
 module.exports = router;
